94065ddc9cfadc96c30b321e12b5b66890754c3c
Lukas Gehreke <lukas.gehreke@gmail.com>
2016-12-04 20:26:35 +0100

    Added method to decrypt rooms

--- Day4/Day4.java
+++ Day4/Day4.java
@@ -9,14 +9,44 @@ public class Day4 {
             Files f = new Files();
             String in = f.readString(new File("/home/lukas/AdventOfCode2016/Day4/puzzleinput"), "UTF8");
             String[] i2 = in.split("\\n");
+            ArrayList<String> valid = createValidList(i2);
+            ArrayList<String> dec = decrypt(valid);
             int sum = 0;
             for (int i = 0; i < i2.length; i++) {
                 sum += checkRoom(i2[i]);
             }
+            for (int i = 0; i < dec.size(); i++) if (dec.get(i).charAt(0) == 'n')System.out.println(dec.get(i));
             System.out.println(sum);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int getSectorID(String s) {
+        String[] parts = s.split("-");
+        String[] ctrl = parts[parts.length - 1].split("\\[");
+        return Integer.parseInt(ctrl[0]);
+    }
+
+    private static ArrayList<String> decrypt(ArrayList<String> s) {
+        String sub;
+        int csum;
+        String temp;
+        int rep;
+        ArrayList<String> re = new ArrayList<>();
+        for (int i = 0; i < s.size(); i++) {
+            sub = s.get(i).replaceAll("\\d+.*", "");
+            sub = sub.replaceAll("-", "");
+            csum = getSectorID(s.get(i));
+            temp = "";
+            for (int j = 0; j < sub.length(); j++) {
+                rep = (((sub.charAt(j) - 97) + csum) % 26) + 97;
+                temp += Character.toString ((char) rep);
+            }
+            temp += " | " + csum;
+            re.add(temp);
+        }
+        return re;
     }
 
     private static String getCommon(String s) {
@@ -41,6 +71,14 @@ public class Day4 {
         return re;
     }
 
+    private static ArrayList<String> createValidList(String[] s) {
+        ArrayList<String> re = new ArrayList<>();
+        for (int i = 0; i < s.length; i++) {
+            if (checkRoom(s[i]) != 0) re.add(s[i]);
+        }
+        return re;
+    }
+
     private static int posMax(int[] in) {
         int max = 0;
         int pos = 0;
@@ -53,7 +91,7 @@ public class Day4 {
         return pos;
     }
 
-    public static int checkRoom(String room) {
+    private static int checkRoom(String room) {
         String[] parts = room.split("-");
         String[] ctrl = parts[parts.length - 1].split("\\[");
         ctrl[1] = ctrl[1].replaceAll("\\]", "");
