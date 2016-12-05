f26d99c9c4ec09b4d035c6f23d98e27fb3d2d6ec
Lukas Gehreke <lukas.gehreke@gmail.com>
2016-12-05 14:32:58 +0100

    Added method to solve part 2

--- Day5/Day5.java
+++ Day5/Day5.java
@@ -4,8 +4,48 @@ import java.security.*;
 public class Day5 {
 
    public static void main(String[] args) {
-       System.out.println(getPassword("ugkcyxxp"));
+       //System.out.println(getPassword("ugkcyxxp"));
+       System.out.println(getPasswordIndex("ugkcyxxp"));
    }
+
+    private static String getString(String[] s) {
+        String re = "";
+        for (int i = 0; i < s.length; i++) re += s[i];
+        return re;
+    }
+
+    private static String getPasswordIndex(String input) {
+        try {
+            String[] s = new String[8];
+            for (int i = 0; i < s.length; i++) s[i] = "_";
+            long c = 0;
+            String hash = input;
+            String t;
+            int pos;
+            System.out.println(getString(s));
+            while (!fin(s)) {
+                t = MD5(hash + c++).toString();
+                if (t.substring(0, 5).equals("00000")){
+                    if (Character.isDigit(t.charAt(5))) {
+                        pos = Integer.parseInt(Character.toString(t.charAt(5)));
+                        if (pos < 8 && s[pos].equals("_")) s[pos] = Character.toString(t.charAt(6));
+                        System.out.println(getString(s));
+                    }
+                }
+            }
+            return getString(s);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "foo";
+        }
+    }
+
+    private static boolean fin(String[] s) {
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("_")) return false;
+        }
+        return true;
+    }
 
     private static String getPassword(String input) {
         try {
