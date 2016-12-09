0ea7189057b35fa989918e542fde815505bd8ee9
Lukas Gehreke <lukas.gehreke@gmail.com>
2016-12-09 19:21:22 +0100

    Added Method for part 2

--- Day9/day9.py
+++ Day9/day9.py
@@ -4,17 +4,16 @@ with open('/home/lukas/AdventOfCode2016/Day9/puzzleinput', 'r') as puin:
 def decompress(string):
 	dec = ''
 	marker = ''
-	#print string
 	i = 0
 	while i < len(string):
 		if string[i] != '(':
 			dec += string[i]
 			i += 1
 		else:
-			marker = getMarker(i+1, string[i+1:])
+			marker = getMarker(i+1, string[i+1:])	
 			i = int(marker[2]) + int(marker[0])
 			for j in range(0, int(marker[1])):
-				dec += string[int(marker[2]):int(marker[2])+int(marker[0])]
+					dec += string[int(marker[2]):int(marker[2])+int(marker[0])]
 	return dec
 
 def getMarker(i, s):
@@ -29,4 +28,19 @@ def getMarker(i, s):
 	mlist.append(nindex)
 	return mlist	
 
+def recDec(string):
+	l = 0
+	if '(' not in string:
+		return len(string)
+	while '(' in string:
+		l += string.find('(')
+		string = string[string.find('('):]
+		marker = string[1:string.find(')')].split('x')
+		string = string[string.find(')') + 1:]
+		l += recDec(string[:int(marker[0])]) * int(marker[1])		
+		string = string[int(marker[0]):]
+	l += len(string)
+	return l	
+
 print(len(decompress(p)))
+print(recDec(p))
