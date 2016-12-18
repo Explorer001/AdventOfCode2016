5a71f06458768c504d31c4a6a4b6638997da175e
Lukas Gehreke <lukas.gehreke@gmail.com>
2016-12-18 23:37:24 +0100

    Expanded bfs. Works for part 2 now

--- Day17/day17.py
+++ Day17/day17.py
@@ -2,24 +2,33 @@ import md5
 
 input = 'veumntbg'
 
+part2 = False
+
 def search():
 	nlist = []
 	visited = []
 	nlist.append([[0,0], input])
 	visited.append([[0,0], input])
+	p1 = ''
+	max = ''
+	fl = 0
 	while len(nlist) > 0:
-		fl = 0
 		node = nlist.pop()
 		pos = getMoves(node)
 		for p in pos:
 			if p not in visited:
 				if p[0] == [3,3]:
-					fl = 1
+					if fl == 0:
+						p1 = p[1][len(input):] 
+						fl = 1
+					else:
+						max = len(p[1][len(input):])
+					continue
 				visited.append(p)
 				nlist.insert(0,p)
-		if fl == 1:
+		if fl == 1 and not part2:
 			break
-	return visited
+	return (p1, max)
 
 def getMoves(node):
 	ha = hash(node[1])
@@ -40,9 +49,13 @@ def getMoves(node):
                         re.append([[node[0][0]-1, node[0][1]],node[1]+d])
 		if d == 'R' and node[0][0]+1 < 4:
                         re.append([[node[0][0]+1, node[0][1]],node[1]+d])
-	return re
+	return re 
 
 def hash(s):
 	return md5.new(s).hexdigest()[:4]
 
-print search().pop()[1][len(input):]
+a,b = search()
+print a
+part2 = True
+a,b = search()
+print b
